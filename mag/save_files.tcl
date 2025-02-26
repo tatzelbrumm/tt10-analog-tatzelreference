tatzelbrumm/tt10-analog-tatzeltapeout

mag/save_files.tcl : script to save mag, gds, lef files after edit

mag/save_files.tcl
@@ -1,0 +1,8 @@
+# Save the layout and export GDS/LEF
+# ----------------------------------
+set TOP_LEVEL_CELL     tt_um_tatzeltapeout
+save ${TOP_LEVEL_CELL}.mag
+file mkdir gds
+gds write gds/${TOP_LEVEL_CELL}.gds
+file mkdir lef
+lef write lef/${TOP_LEVEL_CELL}.lef -hide -pinonly

tatzelbrumm/tt10-analog-tatzelreference

mag/save_files.tcl
@@ -1,0 +1,8 @@
+# Save the layout and export GDS/LEF
+# ----------------------------------
+set TOP_LEVEL_CELL     tt_um_tatzelreference
+save ${TOP_LEVEL_CELL}.mag
+file mkdir gds
+gds write gds/${TOP_LEVEL_CELL}.gds
+file mkdir lef
+lef write lef/${TOP_LEVEL_CELL}.lef -hide -pinonly