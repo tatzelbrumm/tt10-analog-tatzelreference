407635b5d2dc3ae92715d918cf578ce91537b1b9
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2025-09-15 19:27:19 +0200

    src/reference.v : Verilog dummy for reference circuit

---
+++ src/reference.v
@@ -0,0 +1,11 @@
+/* Oguey-Aebischer reference */
+(* blackbox *)
+module reference(
+    input wire vss;
+    input wire vdd;
+    input wire disable;
+    inout wire vbp;
+    inout wire vbn;
+    inout wire vbr;
+);
+endmodule
